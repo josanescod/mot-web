ac0a102a08595c877a1efd4e0fee25fecc55eeca
josan <josanescod@gmail.com>
2020-07-02 20:58:53 +0200

    off_canvas mejorado

--- js/loadessentials.js
+++ js/loadessentials.js
@@ -22,54 +22,90 @@ window.onload = () => {
 function loadNavIndex() {
     let pageNav = document.querySelector('#page-nav');
     pageNav.innerHTML = `
-    <button type="button" data-uk-toggle="target: .navbar-collapse; cls: hidden-up-to-s" class="uk-navbar-toggle uk-hidden@s" data-uk-navbar-toggle-icon></button>
-                <div class="uk-navbar-left" >
-                <div class="navbar-collapse hidden-up-to-s uk-navbar-right" >
-                   
-                        <ul data-uk-toggle="media: @s; cls: uk-navbar-nav uk-padding-remove; mode: media" class="uk-nav uk-nav-primary uk-padding-small">
-                            <li class="uk-active"><a href="index.html">Home</a></li>
-                            <li>
-                                <a href="#">Talleres</a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li class="uk-active"><a href=html/taller1.html>Taller 1: escritura creativa</a></li>
-                                        <li><a href="html/taller2.html">Taller 2: lenguaje inclusivo</a></li>
-                                        <li><a href="html/otaller.html">Taller 3: organiza un taller</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="html/acercade.html">Sobre Mot</a></li>
-                            <li><a href="html/contacto.html">Contacto</a></li>
+    <nav class="uk-navbar-container uk-visible@m" uk-navbar="mode: click">                    
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav">
+                    <li class="uk-active"><a href="index.html">HOME</a></li>
+                    <li>
+                        <a href="#">TALLERES</a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                  <li class="uk-active"><a href="html/taller1.html">Taller 1: escritura creativa</a></li>
+                                  <li><a href="html/taller2.html">Taller 2: lenguaje inclusivo</a></li>
+                                  <li><a href="html/otaller.html">Taller 3: organiza un taller</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="html/acercade.html">SOBRE MOT</a></li>
+                    <li><a href="html/contacto.html">CONTACTO</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- boton oculto en tabletas o superior-->
+        <button type="button" class="uk-navbar-toggle uk-hidden@m" uk-toggle="target: #menu_movil" data-uk-navbar-toggle-icon></button>      
+        <div id="menu_movil" uk-offcanvas="overlay: true">
+            <div class="uk-offcanvas-bar uk-hidden@m">
+                <button class="uk-offcanvas-close" type="button" uk-close></button>
+                <ul class="uk-nav uk-nav-default" uk-nav>                 
+                    <li class="uk-nav-header uk-active"><a href="index.html" style="color:white">HOME</a></li>
+                    <li class="uk-nav-header uk-parent">
+                        <a href="#" style="color:white">Tallers</a>
+                        <ul class="uk-nav-sub">
+                          <li><a href="html/taller1.html" style="color:white">Taller 1 - escritura creativa</a></li>
+                          <li><a href="html/taller2.html" style="color:white">Taller 2 - lenguaje inclusivo</a></li>
+                          <li><a href="html/otaller.html" style="color:white">Taller 3 - organiza un taller</a></li>
                         </ul>
-                    </div>
-                </div>
+                      </li>
+                    <li class="uk-nav-header" ><a href="html/acercade.html" style="color:white">SOBRE MOT</a></li>
+                    <li class="uk-nav-header"><a href="html/contacto.html" style="color:white">CONTACTO</a></li>                   
+                </ul>        
+            </div>
+        </div>
     `;
 }
 
 function loadNav() {
     let pageNav = document.querySelector('#page-nav');
     pageNav.innerHTML = `
-    <button type="button" data-uk-toggle="target: .navbar-collapse; cls: hidden-up-to-s" class="uk-navbar-toggle uk-hidden@s" data-uk-navbar-toggle-icon></button>
-                <div class="uk-navbar-left" >
-                <div class="navbar-collapse hidden-up-to-s uk-navbar-right" >
-                   
-                        <ul data-uk-toggle="media: @s; cls: uk-navbar-nav uk-padding-remove; mode: media" class="uk-nav uk-nav-primary uk-padding-small">
-                            <li class="uk-active"><a href="../index.html">Home</a></li>
-                            <li>
-                                <a href="#">Talleres</a>
-                                <div class="uk-navbar-dropdown">
-                                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                                        <li class="uk-active"><a href=taller1.html>Taller 1: escritura creativa</a></li>
-                                        <li><a href="taller2.html">Taller 2: lenguaje inclusivo</a></li>
-                                        <li><a href="otaller.html">Taller 3: organiza un taller</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="acercade.html">Sobre Mot</a></li>
-                            <li><a href="contacto.html">Contacto</a></li>
+    <nav class="uk-navbar-container uk-visible@m" uk-navbar="mode: click">                    
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav">
+                    <li class="uk-active"><a href="../index.html">HOME</a></li>
+                    <li>
+                        <a href="#">TALLERES</a>
+                        <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                  <li class="uk-active"><a href="taller1.html">Taller 1: escritura creativa</a></li>
+                                  <li><a href="taller2.html">Taller 2: lenguaje inclusivo</a></li>
+                                  <li><a href="otaller.html">Taller 3: organiza un taller</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="acercade.html">SOBRE MOT</a></li>
+                    <li><a href="contacto.html">CONTACTO</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- boton oculto en tabletas o superior-->
+        <button type="button" class="uk-navbar-toggle uk-hidden@m" uk-toggle="target: #menu_movil" data-uk-navbar-toggle-icon></button>      
+        <div id="menu_movil" uk-offcanvas="overlay: true">
+            <div class="uk-offcanvas-bar uk-hidden@m">
+                <button class="uk-offcanvas-close" type="button" uk-close></button>
+                <ul class="uk-nav uk-nav-default" uk-nav>                 
+                    <li class="uk-nav-header uk-active"><a href="../index.html" style="color:white">HOME</a></li>
+                    <li class="uk-nav-header uk-parent">
+                        <a href="#" style="color:white">Tallers</a>
+                        <ul class="uk-nav-sub">
+                          <li><a href="taller1.html" style="color:white">Taller 1 - escritura creativa</a></li>
+                          <li><a href="taller2.html" style="color:white">Taller 2 - lenguaje inclusivo</a></li>
+                          <li><a href="otaller.html" style="color:white">Taller 3 - organiza un taller</a></li>
                         </ul>
-                    </div>
-                </div>
+                      </li>
+                    <li class="uk-nav-header" ><a href="acercade.html" style="color:white">SOBRE MOT</a></li>
+                    <li class="uk-nav-header"><a href="contacto.html" style="color:white">CONTACTO</a></li>                   
+                </ul>        
+            </div>
+        </div>
     `;
 }
 
